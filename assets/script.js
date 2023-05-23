8b354e84f18110a10464527af01f351d644841d9
Loverde <w.andrew.loverde@gmail.com>
2023-05-23 00:40:02 -0500

    fixed High score to increment new scores to local storage

--- assets/script.js
+++ assets/script.js
@@ -14,22 +14,7 @@ console.log(choicesEl);
 var timeCount = 05;
 var timer;
 var score = 0;
-
-// Objects for Questions and Highscores
-var highScore = [
-    {
-        name: "william",
-        score: 420
-    },
-    {
-        name: "andrew",
-        score: 69
-    },
-    {
-        name: "robert",
-        score: 360
-    }
-];
+var counter = 0;
 
 var questions = [
     {
@@ -42,36 +27,51 @@ var questions = [
         answers: ["very", "kinda", "sorta", "Most Definitely"],
         correct: "Most Definitely"
     }
-    
+
 ];
 
 // function to start quiz and hide Start Page
 function startQuiz() {
     startPage.setAttribute("style", "display: none;");
     quizPage.setAttribute("style", "display: block;");
-    
+
     // Starts quiz and timer functions
     startTimer();
     renderQuestion(); // calling counter before allowing user to answer
-    
+
 }
 
 // displays game score screen and highscores list
 function endGame() {
+    var highScore = [];
     quizPage.setAttribute("style", "display: none");
     endPage.setAttribute("style", "display: flex;justify-content: space-around;align-items: center;");
+    nameInput = prompt("Your score is: " + score + "\r\nEnter Name here: ")
+    endPage.children[0].children[1].textContent = "Thanks for playing " + nameInput + "\nYour score is: " + score;
+    //push local storage scores
+    highScore = (JSON.parse(localStorage.getItem("highScore")));
+    if (highScore === null) {
+        highScore = [];
+    }
+    // adds new score to local storage
+    var newScore = {
+        name: nameInput,
+        score: score
+    }
+    highScore.push(newScore);
+    console.log(highScore);
+    localStorage.setItem("highScore", JSON.stringify(highScore));
     getHighScore();
 }
 
-var counter = 0;
 function renderQuestion() {
     questionEl.textContent = questions[counter].question;
-    choicesEl.innerHTML= "";
-    for (var i=0; i < questions[counter].answers.length; i++){
+    choicesEl.innerHTML = "";
+    for (var i = 0; i < questions[counter].answers.length; i++) {
         var option = questions[counter].answers[i];
         var optionButton = document.createElement("button");
         optionButton.setAttribute("value", option);
-        optionButton.textContent = (i+1) +". "+ questions[counter].answers[i];
+        optionButton.textContent = (i + 1) + ". " + questions[counter].answers[i];
         choicesEl.appendChild(optionButton);
     }
 }
@@ -79,9 +79,10 @@ function renderQuestion() {
 function quizClick(event) {
     var buttonEl = event.target;
     console.log(buttonEl);
-    console.log("button pressed: " + buttonEl.value + "\nAnswer: "+ questions[counter].correct );
-    if (questions[counter].correct === buttonEl.value){
+    console.log("button pressed: " + buttonEl.value + "\nAnswer: " + questions[counter].correct);
+    if (questions[counter].correct === buttonEl.value) {
         counter++;
+        score++;
         renderQuestion();
     }
 }
@@ -89,6 +90,10 @@ function quizClick(event) {
 
 // add score to high score Object, sort by highest score to lowest
 function getHighScore() {
+    var highScore = (JSON.parse(localStorage.getItem("highScore")));
+    if (highScore === null) {
+        highScore = [];
+    }
     highScore.sort(function (a, b) {
         return b.score - a.score
     });
